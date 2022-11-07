4eac581d60078896477115d8b442c69a088689d4
Anton Osypchuk <osypchuk.ua@gmail.com>
2022-11-07 22:01:00 +0200

    Text modified

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курс Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курс Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курс Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Learning Git online");
+Console.WriteLine("Learning Git online");
+Console.WriteLine("Learning Git online");
 Console.WriteLine("Курс Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курс Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курс Sana Commerce Software Development Bootcamp");
